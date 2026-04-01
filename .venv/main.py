2ca815876534627215f30209b5e73ffa5996f007
André Tozi Magalhães <andretozi95@gmail.com>
2026-04-01 18:42:03 -0300

    TAREFA TERMINADA!! IMPLEMENTADO O TESTE BASICO QUE CARREGA TODOS OS TRES TIPOS DE FILTROS POSSÍVEIS : AUTOR, ANO, TITULO E AUTOR.

--- .venv/main.py
+++ .venv/main.py
@@ -1,13 +1,5 @@
-from lista_livors import biblioteca
+from lista_livros import biblioteca
 
-filto_livros = [ p for p in biblioteca if ['titulo'] = 'Harry Potter e a Pedra Filosofal']
-print(filto_livros)
+def filto_livros(criterios):
 
-filto_livros = [ p for p in biblioteca if ['autor'] = 'Harper Lee']
-print(filto_livros)
-
-filto_livros = [ p for p in biblioteca if ['ano'] = '1881']
-print(filto_livros)
-
-filto_livros = [ p for p in biblioteca if ['categoria'] = 'Ficção Absurda']
-print(filto_livros)
+    return [livro for livro in biblioteca if all(livro.get(chave) == valor for chave, valor in criterios.items())]
